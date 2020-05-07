210920fcb3c6cc76d4cf306a291cf54960c9126a
Mark Ablovatskii <mark.ablov@gmail.com>
2020-05-07 10:31:35 +0700

    process USB CDC line coding

--- Src/usbd_cdc_if.c
+++ Src/usbd_cdc_if.c
@@ -32,7 +32,13 @@
 
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
-
+USBD_CDC_LineCodingTypeDef LineCoding =
+{
+  115200, /* baud rate*/
+  0x00,   /* stop bits-1*/
+  0x00,   /* parity - none*/
+  0x08    /* nb. of bits 8*/
+};
 /* USER CODE END PV */
 
 /** @addtogroup STM32_USB_OTG_DEVICE_LIBRARY
@@ -225,12 +231,21 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
   /* 6      | bDataBits  |   1   | Number Data bits (5, 6, 7, 8 or 16).          */
   /*******************************************************************************/
     case CDC_SET_LINE_CODING:
-
-    break;
+      LineCoding.bitrate = (uint32_t)(pbuf[0] | (pbuf[1] << 8) | (pbuf[2] << 16) | (pbuf[3] << 24));
+      LineCoding.format = pbuf[4];
+      LineCoding.paritytype = pbuf[5];
+      LineCoding.datatype = pbuf[6];
+      break;
 
     case CDC_GET_LINE_CODING:
-
-    break;
+      pbuf[0] = (uint8_t)(LineCoding.bitrate);
+      pbuf[1] = (uint8_t)(LineCoding.bitrate >> 8);
+      pbuf[2] = (uint8_t)(LineCoding.bitrate >> 16);
+      pbuf[3] = (uint8_t)(LineCoding.bitrate >> 24);
+      pbuf[4] = LineCoding.format;
+      pbuf[5] = LineCoding.paritytype;
+      pbuf[6] = LineCoding.datatype;
+      break;
 
     case CDC_SET_CONTROL_LINE_STATE:
 
