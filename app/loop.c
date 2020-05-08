f92a700fa46870220179279a3ea833ef88b86027
Mark Ablovatskii <mark.ablov@gmail.com>
2020-05-08 10:52:26 +0700

    make code a bit less board-specific (create config for PIN bindings)

--- app/loop.c
+++ app/loop.c
@@ -2,9 +2,10 @@
 #include "usbd_cdc_if.h"
 
 #include "loop.h"
+#include "pins.h"
 
 void loopTick(void) {
-  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_12);
+  HAL_GPIO_TogglePin(PIN_PERIPHERAL_LED, PIN_LED_DATA);
   HAL_Delay(200);
   uint8_t buffer[] = "Hello, World!\r\n";
   CDC_Transmit_FS(buffer, sizeof(buffer));
