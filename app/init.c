b9d9205977c0c36df1e39d363baca25c16ef5d16
Mark Ablovatskii <mark.ablov@gmail.com>
2020-05-11 15:07:41 +0700

    run timers

--- app/init.c
+++ app/init.c
@@ -1,5 +1,9 @@
+#include "main.h"
+#include "tim.h"
 #include "init.h"
 
 void init(void) {
-
+  // Timers to generate clock signals for MCU-4 processor
+  HAL_TIM_Base_Start(&htim9);
+  HAL_TIM_PWM_Start(&htim9, TIM_CHANNEL_1);
 }
