c826b4a4235562e0dfdf5310e5aac02618da101c
Mark Ablovatskii <mark.ablov@gmail.com>
2020-05-12 15:25:43 +0700

    decompose init() function

--- app/init.c
+++ app/init.c
@@ -2,12 +2,14 @@
 #include "tim.h"
 #include "init.h"
 
-void init(void) {
-  // Timers to generate clock signals for MCU-4 processor
+void initProcessorClocks() {
   HAL_TIM_Base_Start(&htim8);
   HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_4);
-
   HAL_TIM_Base_Start(&htim1);
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
   HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_2);
+}
+
+void init(void) {
+  initProcessorClocks();
 }
