d29809001595c74e732a944077472efd13a5a928
Mark Ablovatskii <mark.ablov@gmail.com>
2021-12-13 15:49:34 +0700

    enable UART interface

--- Src/gpio.c
+++ Src/gpio.c
@@ -36,8 +36,6 @@
         * Output
         * EVENT_OUT
         * EXTI
-     PC10   ------> UART4_TX
-     PC11   ------> UART4_RX
 */
 void MX_GPIO_Init(void)
 {
@@ -83,14 +81,6 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PC10 PC11 */
-  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF8_UART4;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
 }
 
--- Src/main.c
+++ Src/main.c
@@ -21,6 +21,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "tim.h"
+#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -93,6 +94,7 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM1_Init();
   MX_TIM8_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   init();
   /* USER CODE END 2 */
