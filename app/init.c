10b0f9b8ca3e54b1f50937682c04e1584883238d
Mark Ablovatskii <mark.ablov@gmail.com>
2022-02-01 11:50:16 +0700

    enable CC interrupts for TIM8 (to catch phi2 falling)

--- app/init.c
+++ app/init.c
@@ -11,11 +11,17 @@
  * Pulse width should be (TIM_Period + 1) * (targetPeriod / timerPeriod) - 1 = 227 * (970ms / 1350ms) - 1 = 162
  * Phase offset should be (TIM_Period + 1) * (targetOffset / timerPeriod) - 1 = 227 * (530ms / 1350ms) - 1 = 88
  *
- * From timing diagram i4004 updates some signals (CM-RAM / CM-ROM) at phi2 falling (from high to low), so we need to have interrupt here
+ * Interrupts:
+ *   TIM1 Update - phi1 raising
+ *   TIM1 CC - phi1 falling
+ *   TIM8 Update - phi2 raising
+ *   TIM8 CC - phi2 falling
+ *
+ * From oscilloscope data, i4004 updates some signals (CM-RAM / CM-ROM) at phi2 falling (from high to low), so we need to have interrupt here
  */
 void initMCS4Clocks() {
   HAL_TIM_Base_Start(&htim8);
-  HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_4);
+  HAL_TIM_PWM_Start_IT(&htim8, TIM_CHANNEL_4);
   HAL_TIM_Base_Start(&htim1);
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
   HAL_TIM_OC_Start(&htim1, TIM_CHANNEL_2);
