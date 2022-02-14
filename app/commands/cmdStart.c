1d112acbdcafa28ed3c2b4f8f3ca8b252563ba38
Mark Ablovatskii <mark.ablov@gmail.com>
2022-02-14 12:53:59 +0700

    initialize ROM with data from UART command

--- app/commands/cmdStart.c
+++ app/commands/cmdStart.c
@@ -1,12 +1,16 @@
 #include "main.h"
 #include "externalInterface.h"
 #include "cycleHandler.h"
+#include "romEmulator.h"
 
 #include "commands.h"
 
 void cmdStart(uint8_t *data, uint32_t dataLen) {
   sendExternalMessage("START command has been received, RAM dump size is %d bytes\r\n", dataLen);
 
+  initROM(data, dataLen);
+
+  // run 4004
   resetCyclerState();
   HAL_GPIO_WritePin(OUT_4004_RESET_GPIO_Port, OUT_4004_RESET_Pin, GPIO_PIN_SET);
 }
