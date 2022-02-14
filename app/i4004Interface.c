5a43367694198250f7a159fa505f5c8e3c5319d7
Mark Ablovatskii <mark.ablov@gmail.com>
2022-02-14 12:53:31 +0700

    add functions to write to i4004 data bus

--- app/i4004Interface.c
+++ app/i4004Interface.c
@@ -1,20 +1,30 @@
 #include "main.h"
 #include "i4004Interface.h"
 
-uint8_t i4004_readDataBus() {
+volatile uint8_t i4004_readDataBus() {
   // for optimization pins are sequential 0..3
   return (IN_4004_D0_GPIO_Port->IDR & 0b0000000000001111U);
 }
 
-uint8_t i4004_readCMRAM() {
+void i4004_writeDataBus(uint8_t data) {
+  // for optimization pins are sequential 3..7 (4 data pins + enable-bus-write)
+  // also need to invert 4-bit data word, because data bus is active low
+  OUT_4004_D0_GPIO_Port->BSRR = (uint32_t)(data << 19U) | OUT_4004_ENABLE_BUS_WRITE_Pin | (uint32_t)((uint8_t)(data ^ 0xFU) << 3U);
+}
+
+void i4004_freeDataBus() {
+  OUT_4004_ENABLE_BUS_WRITE_GPIO_Port->BSRR = (uint32_t)OUT_4004_ENABLE_BUS_WRITE_Pin << 16U;
+}
+
+volatile uint8_t i4004_readCMRAM() {
   // for optimization pins are sequential 4..7
   return (IN_4004_CMRAM0_GPIO_Port->IDR & 0b0000000011110000U) >> 4U;
 }
 
-uint8_t i4004_readCMROM() {
+volatile uint8_t i4004_readCMROM() {
   return (IN_4004_CMROM_GPIO_Port->IDR & IN_4004_CMROM_Pin) != 0;
 }
 
-uint8_t i4004_readSync() {
+volatile uint8_t i4004_readSync() {
   return (IN_4004_SYNC_GPIO_Port->IDR & IN_4004_SYNC_Pin) != 0;
 }
--- app/include/i4004Interface.h
+++ app/include/i4004Interface.h
@@ -7,10 +7,13 @@ extern "C" {
 
 #include <stdint.h>
 
-uint8_t i4004_readDataBus();
-uint8_t i4004_readCMRAM();
-uint8_t i4004_readCMROM();
-uint8_t i4004_readSync();
+volatile uint8_t i4004_readDataBus();
+void i4004_writeDataBus(uint8_t data);
+void i4004_freeDataBus();
+
+volatile uint8_t i4004_readCMRAM();
+volatile uint8_t i4004_readCMROM();
+volatile uint8_t i4004_readSync();
 
 #ifdef __cplusplus
 }
