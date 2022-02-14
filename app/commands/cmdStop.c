0b1899c7caa6f1c0da94b117d56886833b6a2188
Mark Ablovatskii <mark.ablov@gmail.com>
2022-02-14 12:55:59 +0700

    stop cycler on STOP command

--- app/commands/cmdStop.c
+++ app/commands/cmdStop.c
@@ -6,6 +6,7 @@
 
 void cmdStop() {
   HAL_GPIO_WritePin(OUT_4004_RESET_GPIO_Port, OUT_4004_RESET_Pin, GPIO_PIN_RESET);
+  stopCycler();
 
   sendExternalMessage("STOP command has been received\r\n");
   printCyclerStats();
