af703b2c3c53cac74033581b689c2f8867e6e7e1
Mark Ablovatskii <mark.ablov@gmail.com>
2022-02-25 14:23:36 +0700

    disable RTS/CTS control for UART

--- Src/usart.c
+++ Src/usart.c
@@ -37,7 +37,7 @@ void MX_USART3_UART_Init(void)
   huart3.Init.StopBits = UART_STOPBITS_1;
   huart3.Init.Parity = UART_PARITY_NONE;
   huart3.Init.Mode = UART_MODE_TX_RX;
-  huart3.Init.HwFlowCtl = UART_HWCONTROL_RTS_CTS;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart3.Init.OverSampling = UART_OVERSAMPLING_16;
   if (HAL_UART_Init(&huart3) != HAL_OK)
   {
